806321050f0b4f3796e12453a12d0ad1b35241bf
hotel <sparta@hotel>
2021-07-20 21:52:39 +0900

    커밋 테스트2

---
+++ app.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+db = client.bbs
+
+
+# HTML 화면 보여주기
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
