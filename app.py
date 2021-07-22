eb9f3e337a4e7c14fcaf64db4b510c4662a1e77f
bae-code <68859996+bae-code@users.noreply.github.com>
2021-07-22 19:06:49 +0900

    edit 상품관리 button

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from os import name
+from flask_admin.base import expose
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import flask_admin as admin
@@ -22,25 +23,26 @@ db = conn.test
 
 # User admin
 
-class menuForm(form.Form):
+class menu_form(form.Form):
     img = fields.StringField('사진')
     menu = fields.StringField('메뉴')
     price = fields.StringField('가격')
     category = fields.SelectField('카테고리', choices= [('김치', '김치'),
                 ('마른반찬', '마른반찬'), ('나물', '나물'), ('국/탕/찌개', '국/탕/찌개'), ('조림/구이', '조림/구이'), ('튀김/전', '튀김/전'), ('도시락', '도시락'), ('제사/명절','제사/명절')])
     hide = fields.SelectField('숨김/보임', choices= [('0','보임'), ('1','숨김')] )
+
     # DB에 저장할때 사용하는 key = fields.StringField('name') < value 값이 저장되는 inputbox
 
 
 
-class menuView(ModelView):
+class menu_view(ModelView):
 
     column_list = ('img', 'menu', 'price','category') #db에서 불러올때 사용하는 key값
-
     # column_sortable_list = ('img', 'menu', 'price') # list 정렬가능한 컬렉션
 
-    form = menuForm
+    form = menu_form
 
+    
     # def on_model_change(self, form, model, is_created):
     #     user_id = model.get('user_id')
     #     model['user_id'] = ObjectId(user_id)
@@ -76,14 +78,13 @@ def post_test():
 
 
 if __name__ == '__main__':
-    # Create admin
-    admin = admin.Admin(app, name='맘 스 키 친')
-
+    # url 변경할것 
+    admin = admin.Admin(app, name='맘스키친', url='/bluenight')
+    
     # Add views
-    admin.add_view(menuView(db.user, '상품관리'))
+    admin.add_view(menu_view(db.user, '상품관리'))
     # admin.add_view(orderView(db.order, '주문내역'))
     
-
     # Start app
     app.run(debug=True)
 
