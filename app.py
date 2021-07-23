b205db751594f37ed47f835feed2bb8527b322a4
leubenia <hdodo777@gmail.com>
2021-07-23 18:27:16 +0900

    t??

--- app.py
+++ app.py
@@ -1,60 +1,50 @@
 from os import name
-from flask.templating import render_template
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-import json
-from bson.json_util import dumps
-
-from flask import Flask
 import flask_admin as admin
-from pymongo.mongo_client import MongoClient
-
-from wtforms import form, fields
-
-from flask_admin.form import Select2Widget
-from flask_admin.contrib.pymongo import ModelView, filters
-
-from flask import Flask, render_template, jsonify, request
-
-# Create application
-app = Flask(__name__)
 
 # Create dummy secrey key so we can use sessions
 app.config['SECRET_KEY'] = '123456790'
 
 # Create models
-conn = MongoClient('localhost', 27017)
-db = conn.test
+conn = MongoClient()
+db = conn.bdd
 
 
 # User admin
 
-class menuForm(form.Form):
+class menu_form(form.Form):
     img = fields.StringField('사진')
     menu = fields.StringField('메뉴')
     price = fields.StringField('가격')
     category = fields.SelectField('카테고리', choices= [('김치', '김치'),
                 ('마른반찬', '마른반찬'), ('나물', '나물'), ('국/탕/찌개', '국/탕/찌개'), ('조림/구이', '조림/구이'), ('튀김/전', '튀김/전'), ('도시락', '도시락'), ('제사/명절','제사/명절')])
     hide = fields.SelectField('숨김/보임', choices= [('0','보임'), ('1','숨김')] )
+
     # DB에 저장할때 사용하는 key = fields.StringField('name') < value 값이 저장되는 inputbox
 
+class order_form(form.Form):
+    state = fields.SelectField ('주문 상태', choices= [('입금확인', '입금확인'),('배송중','배송중'),('배송완료','배송완료')])
 
 
-class menuView(ModelView):
+class menu_view(ModelView):
 
     column_list = ('img', 'menu', 'price','category') #db에서 불러올때 사용하는 key값
+    
 
-    # column_sortable_list = ('img', 'menu', 'price') # list 정렬가능한 컬렉션
+    form = menu_form
 
-    form = menuForm
+class order_view(ModelView):
+    column_list = ('name', 'menu', 'number', 'address', 'price', 'state' )
+    column_sortable_list = ('name', 'menu', 'number', 'address', 'price', 'state')
+    can_edit = True
 
-    def on_model_change(self, form, model, is_created):
-        user_id = model.get('user_id')
-        model['user_id'] = ObjectId(user_id)
+    form = order_form
 
-        return model
-
-
+    
+    # def on_model_change(self, form, model, is_created):
+    #     user_id = model.get('user_id')
+    #     model['user_id'] = ObjectId(user_id)
 class JSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, ObjectId):
@@ -63,19 +53,21 @@ class JSONEncoder(json.JSONEncoder):
 
 
 
+    #     return model
+    
 # Flask views
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
-@app.route('/header.html')
-def dotest():
-    return render_template('header.html')
+# @app.route('/header.html')
+# def dotest():
+#     return render_template('header.html')
 
-@app.route('/footer.html')
-def do1test():
-    return render_template('footer.html')
+# @app.route('/footer.html')
+# def do1test():
+#     return render_template('footer.html')
 
 # 일단 보류
 # @app.route('/habit_s', methods=['GET'])
@@ -85,14 +77,14 @@ def do1test():
 #     return jsonify({'all_order': orders})
 
 
-@app.route('/mypage')
-def objetdata():
-    return render_template('objectdatap.html')
+# @app.route('/mypage')
+# def objetdata():
+#     return render_template('objectdatap.html')
 
 
-@app.route('/test')
-def test11():
-    return render_template('test.html')
+# @app.route('/test')
+# def test11():
+#     return render_template('test.html')
 
 @app.route('/mypage/do', methods=['GET'])
 def post_test():
@@ -112,13 +104,15 @@ def post_test():
 
 
 if __name__ == '__main__':
-    # Create admin
-    admin = admin.Admin(app, name='맘 스 키 친')
-
-    # Add views
-    admin.add_view(menuView(db.user, '상품관리'))
-    # admin.add_view(orderView(db.order, '주문내역'))
+    # url 변경할것 
+    admin = admin.Admin(app, name='맘스키친', url='/bluenight')
     
+    # Add views
+    admin.add_view(menu_view(db.menu, '상품관리', url='/Product_management'))
+    admin.add_view(order_view(db.user, '주문내역', url='/Order_details'))
 
     # Start app
     app.run(debug=True)
+
+    
+    
